7f217e22d71a792a3b848f3d6098137aae81afc6
pristina05 <pristina.varghese@wohlig.in>
2021-08-13 16:56:27 +0530

    fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <EmployeeList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import EmployeeList from './components/EmployeeList'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    EmployeeList
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,62 @@
+<template>
+        <div>
+                <h1> Employee List </h1>
+                <table id="t01">
+                        <tr>
+                                <th>Name</th>
+                                <th>Salary</th>
+                                <th>Age</th>
+                        </tr>
+                         <tr v-for="item in list" v-bind:key="item.id">
+                                <th>{{item.employee_name}}</th>
+                                <th>{{item.employee_salary}}</th>
+                                <th>{{item.employee_age}}</th>
+                        </tr>
+                </table>
+        </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueAxios from 'vue-axios'; 
+import axios from 'axios';
+Vue.use(VueAxios,axios)
+export default {
+        name:"EmployeeList",
+        data() 
+        {
+                return {
+                        list:undefined
+                }
+        },
+        mounted()
+        {
+                Vue.axios.get('http://dummy.restapiexample.com/api/v1/employees')
+                .then(resp=>{
+                        this.list=resp.data.data
+                        console.warn(resp.data.data)
+                })
+        }
+        
+}
+</script>
+<style scoped>
+table{
+        width:100%;
+}
+table, th, td{
+        border: 1px solid black;
+        border-collapse: collapse;
+}
+th, td{
+        padding: 15px;
+        text-align: left;
+}
+#t01 tr:nth-child(even) {
+  background-color: #eee;
+}
+#t01 tr:nth-child(odd) {
+ background-color: #fff;
+}
+
+</style>
